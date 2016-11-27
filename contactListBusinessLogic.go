15dd0e600e8425ce4f58e7db679def297deb9085
Chaitanya Sri Krishna Lolla <Chaitanya Sri Krishna Lolla>
2016-11-27 12:33:57 -0500

    Updated the Business Logic to check if the file exists or not.

--- contactListBusinessLogic.go
+++ contactListBusinessLogic.go
@@ -21,8 +21,31 @@ import(
 	"log"
 )
 
-//  Create a file in a specified location.
-func createFile(fullPath string){
+var fullPath string = "directory.csv"
+
+/* Function to check if the file exists or not.
+ 1. This function returns true if the file already exits.
+ 2. Else it will create a file in the given path and returns false.
+*/
+func isFileExists(filePath string) bool {
+
+	var isExists bool = false
+	_,err := os.Stat(filePath)
+	if err!= nil {
+		fmt.Println("File doesn't exists in the given directory. A new file will be created.")
+		createFile()
+		// Create The file.
+	} else {
+		fmt.Println("File already Exists.")
+		isExists = true
+	}
+	fullPath = filePath;
+	return isExists
+}
+
+
+//  Create a file in the Location which has been set in the global path.
+func createFile(){
 
 	f,err:= os.Create(fullPath)
 	check(err)
@@ -34,10 +57,10 @@ func createFile(fullPath string){
 	fmt.Println("Succesffully written the file with length : ",n3)
 }
 
-// Write records into the given file.
-func writeRecordIntoFile(dataString string, filename string){
+// Write records into the given file specified in the global path.
+func writeRecordIntoFile(dataString string){
 	
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	f, err := os.OpenFile(fullPath, os.O_APPEND|os.O_WRONLY, 0600)
 	check(err)
 
 	defer f.Close()
@@ -47,15 +70,16 @@ func writeRecordIntoFile(dataString string, filename string){
 	
 }
 
-// Add Records into the give file.
+// Add Records into the give file specified in the global path.
 func addRecordsToTheFile(name string, phoneNumber string,email string, address string, deleteFlag string){
-	id := getRecordNumber("directory.csv")
+	id := getRecordNumber(fullPath)
 	var index string = strconv.Itoa(id)
 	var text string = index+"|"+name+"|"+phoneNumber+"|"+email+"|"+address+"|"+"0"+"\n"
 	fmt.Println(text)
-	writeRecordIntoFile(text,"directory.csv")
+	writeRecordIntoFile(text)
 }
 
+//Helper Function.
 // Indexing Implementation which checks the current position of the record in CSV File.
 func getRecordNumber(filename string) int {
 	bytes,err := ioutil.ReadFile(filename)
@@ -67,8 +91,8 @@ func getRecordNumber(filename string) int {
 }
 
 // Retrieve the CSV contact data and form a front end data.
-func retrieveContactData(filename string) map[string][]string{
-	bytes,err := ioutil.ReadFile(filename)
+func retrieveContactData() map[string][]string{
+	bytes,err := ioutil.ReadFile(fullPath)
 	check(err)
 	var data string = string(bytes)
 
@@ -81,8 +105,9 @@ func retrieveContactData(filename string) map[string][]string{
 	for i:=1; i<len(records);i++ {
 		str:= records[i]
 		newstr:= s.Split(str,"|")
-		deleteFlag := newstr[len(newstr)-1]
-		if(s.Compare(deleteFlag,"0")==0){
+		//deleteFlag := newstr[len(newstr)-1]
+		//TODO: No need for the condition.
+		//if(s.Compare(deleteFlag,"0")==0){
 
 			finalstr:= s.Join(newstr,";")
 			if(count==1){
@@ -91,7 +116,7 @@ func retrieveContactData(filename string) map[string][]string{
 				mapStr = append(mapStr,finalstr)
 			}
 			count+=1;
-		}
+		//}
 
 	}
 	dataToBeSent["contacts"] = mapStr
@@ -101,15 +126,15 @@ func retrieveContactData(filename string) map[string][]string{
 }
 
 // Logical deletion of the Contact Data.
-func deleteRecord(filename string,id string){
+func deleteRecord(id string){
 
-	bytes,err := ioutil.ReadFile(filename)
+	bytes,err := ioutil.ReadFile(fullPath)
 	check(err)
 	var data string = string(bytes)
 
 	records := s.Split(data,"\n")
 
-	createFile("directory.csv")
+	createFile()
 	// Form the Front end data.
 	//count := 1;
 	for i:=1; i<len(records);i++ {
@@ -122,20 +147,20 @@ func deleteRecord(filename string,id string){
 
 		finalstr:= s.Join(newstr,"|")
 		finalstr = finalstr+"\n"
-		writeRecordIntoFile(finalstr,filename)
+		writeRecordIntoFile(finalstr)
 	}
 }
 
 // Update Records against a specific Field given.
-func updateRecord(filename string, id string, targetstr string, targetval string){
+func updateRecord(id string, targetstr string, targetval string){
 
-	bytes,err := ioutil.ReadFile(filename)
+	bytes,err := ioutil.ReadFile(fullPath)
 	check(err)
 	var data string = string(bytes)
 
 	records := s.Split(data,"\n")
 
-	createFile("directory.csv")
+	createFile()
 	// Form the Front end data.
 	//count := 1;
 	for i:=1; i<len(records);i++ {
@@ -156,7 +181,7 @@ func updateRecord(filename string, id string, targetstr string, targetval string
 
 		finalstr:= s.Join(newstr,"|")
 		finalstr = finalstr+"\n"
-		writeRecordIntoFile(finalstr,filename)
+		writeRecordIntoFile(finalstr)
 	}
 }
 
@@ -170,10 +195,11 @@ func check(e error) {
 
 // Main function implementation for the busines Logic.
 func main() {
-	 	
+	 
+	 isFileExists("contactlist.csv")
 	//createFile("directory.csv")
-	//addRecordsToTheFile("Chaitanya Lolla","9803187958","lollachaitanya@yahoo.com","UT","0")
-	//retrieveContactData("directory.csv")
-	//updateRecord("directory.csv","1","name","Chennu")
-	deleteRecord("directory.csv","1")
+	//addRecordsToTheFile("Chaitanya Sri Krishna Lolla","9803187958","lollachaitanya@gmail.com","UT","0")
+	retrieveContactData()
+	//updateRecord("1","name","Chennu")
+	//deleteRecord("1")
 }	
